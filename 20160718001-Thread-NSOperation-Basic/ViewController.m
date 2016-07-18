f17fa1ef87f0f52b82cb0b1e20c7caf3129c18ba
Rainer <it_jinlei.yue@163.com>
2016-07-18 16:36:46 +0800

    线程知识点梳理：NSOperation的基本使用
    线程知识点梳理：NSOperation的基本使用

--- 20160718001-Thread-NSOperation-Basic/ViewController.m
+++ 20160718001-Thread-NSOperation-Basic/ViewController.m
@@ -16,12 +16,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//    [self invocationOperationFuction];
+    
+    [self blockOperationFunction];
+}
+
+- (void)invocationOperationFuction {
+    // 创建并初始化一个队列对象
+    NSInvocationOperation *invocationOperation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(run:) object:@"test"];
+    
+    // 开启队列
+    [invocationOperation start];
+}
+
+/**
+ *  block方式执行队列
+ */
+- (void)blockOperationFunction {
+    // block方式创建一个队列对象，这里是在主线程中
+    NSBlockOperation *blockOperation = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"test run in %@", [NSThread currentThread]);
+    }];
+    
+    // 以下addExecutionBlock均在不同的子线程中执行
+    [blockOperation addExecutionBlock:^{
+        NSLog(@"test2 run in %@", [NSThread currentThread]);
+    }];
+    
+    [blockOperation addExecutionBlock:^{
+        NSLog(@"test3 run in %@", [NSThread currentThread]);
+    }];
+    
+    [blockOperation addExecutionBlock:^{
+        NSLog(@"test4 run in %@", [NSThread currentThread]);
+    }];
+    
+    [blockOperation start];
+}
+
+/**
+ *  队列执行的方法
+ */
+- (void)run:(NSString *)param {
+    NSLog(@"test run in %@", [NSThread currentThread]);
 }
 
 @end
